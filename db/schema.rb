f92298e4d5bdf363a0dbd3fe2986ce0b6399d130
David Krentzlin <david.krentzlin@xing.com>
2022-05-12 09:14:25 +0200

    make sure packages can have specs and everything works

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,5 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_11_131538) do
+  create_table "posts", force: :cascade do |t|
+    t.string "message", limit: 250, null: false
+    t.integer "likes_count", default: 0, null: false
+    t.integer "repost_count", default: 0, null: false
+    t.integer "users_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["users_id"], name: "index_posts_on_users_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", limit: 300, null: false
+    t.string "handle", limit: 200, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["handle"], name: "index_users_on_handle", unique: true
+  end
+
+  add_foreign_key "posts", "users", column: "users_id"
 end
